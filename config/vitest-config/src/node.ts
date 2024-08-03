61bad356f6789155520346b5c78a70c4721facfc
Estevão <estevao.biondi@gmail.com>
2024-08-03 10:44:51 -0300

    feat(config/vitest-config): create pkg

---
+++ config/vitest-config/src/node.ts
@@ -0,0 +1,12 @@
+import tsConfigPaths from 'vite-tsconfig-paths'
+import type { UserConfig } from 'vitest/config'
+
+const config: UserConfig = {
+	test: {
+		globals: true,
+		root: './',
+	},
+	plugins: [tsConfigPaths()],
+}
+
+export default config
