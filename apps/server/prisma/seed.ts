61c2374391403a5e15d2520b14a9726ba78ef25c
Estevão <estevao.biondi@gmail.com>
2024-09-10 21:17:07 -0300

    feat(apps/server): add prisma seed

---
+++ apps/server/prisma/seed.ts
@@ -0,0 +1,23 @@
+import { faker } from '@/test/lib/faker'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function seed() {
+	await prisma.instance.deleteMany()
+
+	await prisma.instance.create({
+		data: {
+			name: faker.company.name(),
+			phone: '5511975513068',
+		},
+	})
+}
+
+seed()
+	.catch(async (error) => {
+		console.error(error)
+	})
+	.finally(async () => {
+		await prisma.$disconnect()
+	})
