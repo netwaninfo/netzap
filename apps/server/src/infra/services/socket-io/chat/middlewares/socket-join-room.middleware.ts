f11fb9b3a673793176c230e868876b1024e7b0a7
Estevão <estevao.biondi@gmail.com>
2024-10-14 19:45:48 -0300

    fix(server): change to use `WsException`

--- apps/server/src/infra/services/socket-io/chat/middlewares/socket-join-room.middleware.ts
+++ apps/server/src/infra/services/socket-io/chat/middlewares/socket-join-room.middleware.ts
@@ -1,5 +1,7 @@
+import { WsException } from '@nestjs/websockets'
 import { type Socket } from 'socket.io'
 import { z } from 'zod'
+import { fromZodError } from 'zod-validation-error'
 
 import { Middleware } from '../../decorators/middleware.decorator'
 import { SocketMiddleware, SocketMiddlewareNext } from '../../types/middleware'
@@ -12,11 +14,16 @@ const querySchema = z.object({
 export class SocketJoinRoomMiddleware implements SocketMiddleware {
   execute(socket: Socket, next: SocketMiddlewareNext) {
     const rawQuery = socket.handshake.query
-    const query = querySchema.parse(rawQuery)
+    const parsed = querySchema.safeParse(rawQuery)
 
+    if (!parsed.success) {
+      throw new WsException(fromZodError(parsed.error))
+    }
+
+    const query = parsed.data
     const roomId = query.instanceId
-    socket.join(roomId)
 
+    socket.join(roomId)
     socket.handshake.query = Object.assign(rawQuery, query)
 
     return next()
--- apps/server/src/infra/services/socket-io/socket.module.ts
+++ apps/server/src/infra/services/socket-io/socket.module.ts
@@ -1,17 +1,9 @@
 import { Module } from '@nestjs/common'
-import { APP_FILTER } from '@nestjs/core'
 
 import { SocketChatModule } from './chat/socket-chat.module'
-import { SocketExceptionsFilter } from './socket-exception-filter'
 
 @Module({
   imports: [SocketChatModule],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: SocketExceptionsFilter,
-    },
-  ],
   exports: [SocketChatModule],
 })
 export class SocketModule {}
