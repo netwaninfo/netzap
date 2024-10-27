efc4b6f2e434c3036c91a4861a2c949b5a0ad71c
Estevão <estevao.biondi@gmail.com>
2024-10-27 13:10:21 -0300

    feat(server): create route from import from instance

---
+++ apps/server/src/infra/http/controllers/chat/instances/import-from-instance.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, HttpCode, HttpStatus, Param, Post } from '@nestjs/common'
+import {
+  type ImportFromInstanceParams,
+  importFromInstanceParamsSchema,
+} from '@netzap/http/chat'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ImportFromInstanceUseCase } from '@/domain/chat/application/use-cases/instances/import-from-instance-use-case'
+import { ZodHttpValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
+
+const paramsSchema = new ZodHttpValidationPipe(importFromInstanceParamsSchema)
+
+@Controller('/wa/instances/:instanceId/contacts')
+export class ImportFromInstanceController {
+  constructor(private importFromInstance: ImportFromInstanceUseCase) {}
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async handle(
+    @Param(paramsSchema) params: ImportFromInstanceParams
+  ): Promise<void> {
+    const { instanceId } = params
+
+    await this.importFromInstance.execute({
+      instanceId: UniqueEntityID.create(instanceId),
+    })
+  }
+}
