1a475711d9a060bbca49874a5b8cd943af6e0bcf
Estevão <estevao.biondi@gmail.com>
2024-11-07 07:53:45 -0300

    refactor(server): use `zod` to validate `clerk` user

--- apps/server/src/infra/auth/sso/clerk/repositories/clerk-users-repository.ts
+++ apps/server/src/infra/auth/sso/clerk/repositories/clerk-users-repository.ts
@@ -3,35 +3,18 @@ import {
   UsersRepositoryFindUniqueBySSOParams,
 } from '@/domain/auth/application/repositories/users-repository'
 import { User } from '@/domain/auth/enterprise/entities/user'
-import { InvalidResourceFormatError } from '@/domain/shared/errors/invalid-resource-format'
 import { User as RawClerkUser } from '@clerk/backend'
 import { Injectable } from '@nestjs/common'
 import { ClerkService } from '../clerk.service'
 import { ClerkUserMapper } from '../mappers/clerk-user-mapper'
-import { ClerkUser, ClerkUserPublicMetadata } from '../types/clerk-user'
+import { ClerkUser, clerkUserSchema } from '../types/clerk-user'
 
 @Injectable()
 export class ClerkUsersRepository implements UsersRepository {
   constructor(private clerk: ClerkService) {}
 
-  private checkIsValidClerkUser(user: RawClerkUser): ClerkUser {
-    const email = user.primaryEmailAddress?.emailAddress
-    const name = user.firstName || user.lastName || user.fullName
-
-    const publicMetadata =
-      user.publicMetadata as unknown as ClerkUserPublicMetadata
-    const refId = publicMetadata.applications.netzap?.id
-
-    if (!email || !name || !refId) {
-      throw new InvalidResourceFormatError({ id: user.id })
-    }
-
-    return {
-      id: user.id,
-      refId,
-      email,
-      name,
-    }
+  private checkIsValidClerkUser(raw: RawClerkUser): ClerkUser {
+    return clerkUserSchema.parse(raw)
   }
 
   async findUniqueBySSO({
--- apps/server/src/infra/auth/sso/clerk/types/clerk-user.ts
+++ apps/server/src/infra/auth/sso/clerk/types/clerk-user.ts
@@ -1,14 +1,26 @@
-export interface ClerkUserPublicMetadata {
-  applications: {
-    netzap?: {
-      id: string
-    }
-  }
-}
+import { z } from 'zod'
 
-export type ClerkUser = {
-  id: string
-  refId: string
-  name: string
-  email: string
-}
+export const clerkUserSchema = z
+  .object({
+    id: z.string(),
+    email: z.object({ emailAddress: z.string().email() }),
+    firstName: z.string().nullable(),
+    lastName: z.string().nullable(),
+    fullName: z.string().nullable(),
+    publicMetadata: z.object({
+      applications: z.object({
+        netzap: z.object({
+          id: z.string(),
+        }),
+      }),
+    }),
+  })
+  .refine(raw => raw.firstName || raw.lastName || raw.fullName)
+  .transform(raw => ({
+    id: raw.id,
+    email: raw.email.emailAddress,
+    name: String(raw.firstName || raw.lastName || raw.fullName),
+    refId: raw.publicMetadata.applications.netzap.id,
+  }))
+
+export type ClerkUser = z.output<typeof clerkUserSchema>
